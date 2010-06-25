44592fbeb4b78b5c6ee576b70223cd749b0cd344
Tom Taylor <tom@tomtaylor.co.uk>
2010-06-25 14:57:31 +0100

    Initial proof of concept.

---
+++ readme.md
@@ -0,0 +1,10 @@
+Thelma
+======
+
+Thelma runs the Readability bookmarklet on the server-side in a node.js server. It's a proof-of-concept.
+
+You'll need jsdom and node-htmlparser in your node.js path; typically ~/.node_libraries.
+
+Then just, you know, run it with `node thelma.js`.
+
+Only tested with node 0.1.99.
---
+++ thelma.js
@@ -0,0 +1,32 @@
+var sys = require("sys"),
+    fs  = require("fs");
+    
+var dom = require("jsdom/lib/jsdom/level1/core").dom.level1.core;
+var window = require("jsdom/lib/jsdom/browser").windowAugmentation(dom).window;
+var Script = process.binding('evals').Script;
+
+var html = fs.readFileSync(__dirname + "/test.html");
+window.document.innerHTML = html;
+
+// these two functions don't seem to be present in DOM level 1, but implementing them as below doesn't seem to break anything - their effects are purely visual
+window.document.styleSheets = function() { return []; };
+window.scrollTo = function(x, y) { return true; };
+
+var readConvertLinksToFootnotes = true;
+var readStyle = 'style-newspaper';
+var readSize = 'size-medium';
+var readMargin = 'margin-wide'
+
+fs.readFile(__dirname + "/readability.js", function(err, data) {
+    Script.runInNewContext(data.toString(), {
+      window: window, 
+      location: window.location, 
+      navigator: window.navigator, 
+      document:window.document,
+      // these three variables are set from the bookmarklet, so we have to define them
+      readStyle: 'style-newspaper', 
+      readSize: 'size-medium', 
+      readMargin: 'margin-wide' 
+    });
+    sys.log(window.document.innerHTML);
+});
